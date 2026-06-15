0a0572f69bb019c69b8c6f62fd7aacd61b09f5e7
TheUthman <the.uthmanadebola@gmail.com>
2026-06-15 07:21:40 +0100

    changes

--- src/services/authservice.js
+++ src/services/authservice.js
@@ -1,21 +1,23 @@
 import api from "../api";
 
-export const register = async (data) => {
-  try {
-    const res = await api.post("/auth/register", data);
-    return res.data;
-  } catch (error) {
-    console.error("Registration error:", {
-      status: error.response?.status,
-      message: error.response?.data?.message || error.message,
-      data: error.response?.data,
-    });
-    throw error;
-  }
+
+export const login = async (credentials) => {
+  const response = await api.post("/auth/login", credentials);
+  return response.data;
 };
 
-export const login = async (data) => {
-  const res = await api.post("/auth/login", data);
 
-  return res.data;
+export const register = async (userData) => {
+  const response = await api.post("/auth/register", userData);
+  return response.data;
+};
+
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export const getCurrentUser = () => {
+  const token = localStorage.getItem("token");
+  return token ? { token } : null;
 };
